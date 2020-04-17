87f704773f937cf5104d595a6e54e21bea371545
Michael <46861284+yamakoto120@users.noreply.github.com>
2020-04-17 10:21:01 +0300

    ah

--- video-bg-play-content.js
+++ video-bg-play-content.js
@@ -4,11 +4,12 @@ const IS_YOUTUBE = window.location.hostname.search(/(?:^|.+\.)youtube.com/) > -1
                    window.location.hostname.search(/(?:^|.+\.)youtube-nocookie.com/) > -1;
 const IS_MOBILE_YOUTUBE = window.location.hostname == 'm.youtube.com';
 const IS_DESKTOP_YOUTUBE = IS_YOUTUBE && !IS_MOBILE_YOUTUBE;
+const IS_VIMEO = window.location.hostname.search(/(?:^|.+\.)vimeo.com/) > -1;
 
 const IS_ANDROID = window.navigator.userAgent.indexOf('Android') > -1;
 
 // Page Visibility API
-if (IS_ANDROID) {
+if (IS_ANDROID || !IS_DESKTOP_YOUTUBE) {
   Object.defineProperties(document.wrappedJSObject,
     { 'hidden': {value: false}, 'visibilityState': {value: 'visible'} });
 }
@@ -16,10 +17,15 @@ if (IS_ANDROID) {
 window.addEventListener(
   'visibilitychange', evt => evt.stopImmediatePropagation(), true);
 
+// Fullscreen API
+if (IS_VIMEO) {
+  window.addEventListener(
+    'fullscreenchange', evt => evt.stopImmediatePropagation(), true);
+}
 
 // User activity tracking
 if (IS_YOUTUBE) {
-  const refreshInterval = 1 * 10 * 1000; // every 5 minutes
+  const refreshInterval = 5 * 60 * 1000; // every 5 minutes
   waitForYoutubeLactInit(() => refreshLact(), refreshInterval);
 }
 
